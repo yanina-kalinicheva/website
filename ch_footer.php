d6b6aa8d0137196682896f9e8a210ebbf89bdd3e
Yanina Kalinicheva <rovaniemi@list.ru>
2017-03-28 22:47:26 +0800

    add chinese footer

---
+++ ch_footer.php
@@ -0,0 +1,50 @@
+<footer class="footer-widgets">
+    <div class="container">
+        <div class="row section" style="padding-top: 50px; padding-bottom: 50px;">
+            <!-- Contacts -->
+            <div class="col-md-6 col-sm-4 mb-sm-100" style="margin-bottom:0px">
+                <div class="widget about-widget">
+                    <h5 class="header-widget">聯絡我們</h5>
+                    <p>地址: 臺灣，台北市信義路五段2號9樓., 電話 (+886-2) 8780-3011, 傳真 (+886-2) 8780-2511, info@mtc.org.tw</p>
+                    <a href="contacts.php">地圖</a>
+                    <ul class="social-links">
+                        <li><a href="https://www.facebook.com/mtcintaipei"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/mtcintaipei"><i class="fa fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+                <!-- / .widget -->
+            </div>
+            <!-- / .col-md-3 -->
+            <!-- Twitter Feed -->
+            <div class="col-md-6 col-sm-4 mb-sm-100 style=" margin-bottom:0px ">
+			  <div id="container" class="widget twitter-widget ">
+				<h5 class="header-widget ">Twitter Feed</h5>
+				<ul>
+				  <a class="twitter-timeline" href="https://twitter.com/mtcintaipei"  data-width="500" data-height="200">Tweets by mtcintaipei</a>
+				  <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>		
+				</ul>
+			  </div><!-- / .widget -->
+			</div><!-- / .col-md-3 -->
+		</div><!-- / .row -->
+	</div><!-- / .container -->
+
+	<!-- Copyright -->
+	<div class="copyright ">
+	  <div class="container ">
+		<div class="row ">
+		
+		<div class="col-sm-9 " style="font-size: 10px ">
+		  <small>版版權所有 © 1996-2017. All rights reserved. 非經授權禁止轉載.<br>
+				 Website design, development and support: yanina.kalinicheva@gmail.com<br> 
+				 </small>
+		</div>
+
+		<div class="col-sm-3 ">
+		  <small><a href="#page-top " class="pull-right to-the-top " style="font-size: 15px " >向上<i class="fa fa-angle-up "></i></a></small>
+		</div>
+
+		</div><!-- / .row -->
+	  </div><!-- / .container -->
+	</div><!-- / .copyright -->
+
+</footer><!-- / .footer-widgets -->
