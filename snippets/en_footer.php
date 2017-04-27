af1697aaf2a222893e4ace30f72d8ce67e8d0e8b
Yanina Kalinicheva <rovaniemi@list.ru>
2017-04-27 17:16:26 +0800

    Turn footers to php

---
+++ snippets/en_footer.php
@@ -0,0 +1,54 @@
+<footer class="footer-widgets">
+  <div class="container">
+	<div class="row section" style ="padding-top: 50px; padding-bottom: 50px;">
+	  <!-- Contacts -->
+	  <div class="col-md-6 col-sm-4 mb-sm-100" style="margin-bottom:0px">
+		<div class="widget about-widget">
+		  <h5 class="header-widget">Contacts</h5>
+		  <p>Address: Taiwan, Taipei, Fl.9, No.2, Sec.5, Hsinyi Rd., tel. (+886-2) 8780-3011, fax. (+886-2) 8780-2511, info@mtc.org.tw</p>
+		  <a href="contacts.php">Map</a>
+
+		  <ul class="social-links">
+			<li><a href="https://www.facebook.com/mtcintaipei"><i class="fa fa-facebook"></i></a></li>
+			<li><a href="https://twitter.com/mtcintaipei"><i class="fa fa-twitter"></i></a></li>
+		  </ul>
+		</div><!-- / .widget -->
+	  </div><!-- / .col-md-3 -->
+
+	  <!-- Twitter Feed -->
+	  <div class="col-md-6 col-sm-4 mb-sm-100 style="margin-bottom:0px">
+		<div class="widget twitter-widget">
+		  <h5 class="header-widget">Twitter Feed</h5>
+		  <ul>
+
+			<a class="twitter-timeline" href="https://twitter.com/mtcintaipei"  data-width="500"  
+			data-height="200">Tweets by mtcintaipei</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>				
+  
+		  </ul>
+		</div><!-- / .widget -->
+	  </div><!-- / .col-md-3 -->
+
+
+	</div><!-- / .row -->
+  </div><!-- / .container -->
+
+  <!-- Copyright -->
+  <div class="copyright">
+	<div class="container">
+	  <div class="row">
+		
+		<div class="col-sm-9" style="font-size: 10px">
+		  <small> Copyright © 1996-2017. All rights reserved. All content might be freely used when provided with link back to this site.<br>
+				  Website design, development and support: yanina.kalinicheva@gmail.com<br> 
+				  </small>
+		</div>
+
+		<div class="col-sm-3">
+		  <small><a href="#page-top" class="pull-right to-the-top">To the top<i class="fa fa-angle-up"></i></a></small>
+		</div>
+
+	  </div><!-- / .row -->
+	</div><!-- / .container -->
+  </div><!-- / .copyright -->
+
+</footer><!-- / .footer-widgets -->
---
+++ snippets/ru_footer.php
@@ -0,0 +1,54 @@
+<footer class="footer-widgets">
+  <div class="container">
+	<div class="row section" style ="padding-top: 50px; padding-bottom: 50px;">
+	  <!-- Contacts -->
+	  <div class="col-md-6 col-sm-4 mb-sm-100" style="margin-bottom:0px">
+		<div class="widget about-widget">
+		  <h5 class="header-widget">Контакты</h5>
+		  <p>11049 Тайвань, Тайбэй, ул. Синьи, секция 5, номер 2, этаж 9. Тел. (+886-2) 8780-3011, факс (+886-2) 8780-2511, эл.почта info@mtc.org.tw</p>
+		  <a href="contacts.php">Карта</a>
+
+		  <ul class="social-links">
+			<li><a href="https://www.facebook.com/mtcintaipei"><i class="fa fa-facebook"></i></a></li>
+			<li><a href="https://twitter.com/mtcintaipei"><i class="fa fa-twitter"></i></a></li>
+		  </ul>
+		</div><!-- / .widget -->
+	  </div><!-- / .col-md-3 -->
+
+	  <!-- Twitter Feed -->
+	  <div class="col-md-6 col-sm-4 mb-sm-100 style="margin-bottom:0px">
+		<div class="widget twitter-widget">
+		  <h5 class="header-widget">Лента Твиттера</h5>
+		  <ul>
+			
+			<a class="twitter-timeline" href="https://twitter.com/mtcintaipei"  data-width="500"  
+			data-height="200">Tweets by mtcintaipei</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>		
+
+		  </ul>
+		</div><!-- / .widget -->
+	  </div><!-- / .col-md-3 -->
+
+
+	</div><!-- / .row -->
+  </div><!-- / .container -->
+
+  <!-- Copyright -->
+  <div class="copyright">
+	<div class="container">
+	  <div class="row">
+		
+		<div class="col-sm-9" style="font-size: 10px">
+		  <small>Copyright © 1996-2017. Все права защищены. При использовании материалов ссылка на сайт Представительства МТК обязательна.<br>
+				 Дизайн, разработка и поддержка сайта: yanina.kalinicheva@gmail.com<br> 
+				 </small>
+		</div>
+
+		<div class="col-sm-3">
+		  <small><a href="#page-top" class="pull-right to-the-top">Вверх<i class="fa fa-angle-up"></i></a></small>
+		</div>
+
+	  </div><!-- / .row -->
+	</div><!-- / .container -->
+  </div><!-- / .copyright -->
+
+</footer><!-- / .footer-widgets -->
